fbf1b6040765f1bfd22546f054b30d677c155dda
Erick Gomes <erick@ceste.com.br>
2025-04-20 08:38:51 -0300

    cria banco de dados local

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = 'sua_chave_secreta'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    from .routes import routes
+    from app.routes import routes
     app.register_blueprint(routes)
 
     return app
