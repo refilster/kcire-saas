8b95d391477d63cd4177d5ac5099f16008be3fda
Erick Gomes <erick@ceste.com.br>
2025-04-19 22:32:02 -0300

    importa o app corretamente para rodar no Render

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+from app.routes import app
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Pega a porta do ambiente ou usa 5000
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
